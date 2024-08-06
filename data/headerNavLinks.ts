17f3cab9f90e355c45d960d660efaba6d870ab32
Jeydin21 <JeyPham21@gmail.com>
2024-08-05 23:33:31 -0400

    Add Umami analytics

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -4,6 +4,7 @@ const headerNavLinks = [
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: 'https://cloud.umami.is/share/82zeCVpzUCVdfDoB/blog.j21.dev', title: '📊' },
 ]
 
 export default headerNavLinks
