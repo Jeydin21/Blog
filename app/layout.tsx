549c30bad9eafe2fac112fd5af3a9b785e4884da
Jeydin21 <JeyPham21@gmail.com>
2024-08-06 20:57:25 -0400

    Add transition duration to theme switch

--- app/layout.tsx
+++ app/layout.tsx
@@ -95,7 +95,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
-      <body className="bg-[#F2F2F2] pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-[#F2F2F2]">
+      <body className="bg-[#F2F2F2] pl-[calc(100vw-100%)] text-[#222222] antialiased dark:bg-gray-950 dark:text-[#F2F2F2] transition-all duration-300">
         <ThemeProviders>
           {/* <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} /> */}
           <Script
